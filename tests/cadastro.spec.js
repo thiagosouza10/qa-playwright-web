4fbd056607c45ae801851c7e62e199608a14337d
Thiago de Souza <thiagodesouza22@gmail.com>
2023-09-02 15:43:38 -0300

    Design Patters - Page Objects

--- tests/cadastro.spec.js
+++ tests/cadastro.spec.js
@@ -1,68 +1,83 @@
-const { test, expect } = require('@playwright/test')
-let faker = require('faker-br')
+const { test } = require('@playwright/test')
+const { HomePage } = require('../utilidades/pages/home')
+const { DeliverPage } = require('../utilidades/pages/deliver')
+const { Utilidades } = require('../utilidades/utilidades')
 
 
 test.describe('Cadastro de entregas Buger Eats', async () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
+    const homePage = new HomePage(page)
+    await homePage.acessarTelaCadastro()
   })
 
   test('Deve cadastrar entregador para o método Moto', async ({ page }) => {
-    //Dados do usuário
-    const _fakerCpf = faker.br.cpf()
-    const _fakerNomeCompleto = `${faker.name.firstName()} ${faker.name.lastName()}`
-    const _fakerEmail = faker.internet.email()
-    // Telefone está sendo gerado dessa forma para poder mostrar array no projeto
-    const _dddsValidos = ['11', '21', '31', '41', '48', '61']
-    const _dddEstado = faker.random.arrayElement(_dddsValidos)
-    const _numeroTelefone = faker.random.number({ min: 10000000, max: 99999999 })
-    const _fakerTelefone = `(${_dddEstado})9${_numeroTelefone}`
+    const deliverPage = new DeliverPage(page)
+    const utilidades = new Utilidades(page)
 
-    //Dados endereço
-    const _fakerCep = faker.address.zipCodeValidByState()
-    //A função match() retorna um array contendo todas as correspondências encontradas
-    // e o método join('') combina todos os dígitos encontrados em uma única string, eliminando quaisquer espaços ou outros caracteres não numéricos.
-    const _fakerNumero = faker.address.streetAddress().match(/\d+/g).join('')
-    const _fakerComplemento = `Apto: ${faker.random.number({ min: 1, max: 300 })} Bloco: ${faker.random.number({ min: 1, max: 2 })}`
+    const fakerUsuario = await utilidades.gerarDadosDoUsuario()
+    const fakerEndereco = await utilidades.gerarDadosEndereco()
 
-    await test.step('acessa tela de cadastro', async () => {
-      await expect(page.locator('main > h1')).toContainText('Seja um parceiro entregador pela Buger Eats')
-      await page.locator('a > strong').click()
-      await expect(page.locator('form > h1')).toContainText('Cadastre-se para')
+    await deliverPage.preencherDadosDoUsuario({
+      nomeCompleto: fakerUsuario.nomeCompleto,
+      cpf: fakerUsuario.cpf,
+      email: fakerUsuario.email,
+      telefone: fakerUsuario.telefone
     })
-
-    await test.step('preenche os dados do usuário', async () => {
-      await page.locator('input[placeholder="Nome completo"]').type(_fakerNomeCompleto)
-      await page.locator('input[placeholder="CPF somente números"]').type(_fakerCpf)
-      await page.locator('input[placeholder="E-mail"]').type(_fakerEmail)
-      await page.locator('input[name="whatsapp"]').type(_fakerTelefone)
+    await deliverPage.preencherDadosDoEndereco({
+      cep: fakerEndereco.cep,
+      numero: fakerEndereco.numero,
+      complemento: fakerEndereco.complemento
     })
+    await deliverPage.selecionarMetodoEntrega('Moto')
+    await deliverPage.efetuarUploadCnh('utilidades/imagem/cnh.jpg')
+    await deliverPage.efetuarCadastro()
+  })
 
-    await test.step('preenche endereço do usuário', async () => {
-      await page.locator('input[placeholder="CEP"]').type(_fakerCep)
-      await page.locator('input[value="Buscar CEP"]').click()
-      await page.waitForTimeout(1000)
-      await page.locator('input[placeholder="Número"]').type(_fakerNumero)
-      await page.locator('input[placeholder="Complemento"]').type(_fakerComplemento)
-    })
+  test('Deve cadastrar entregador para o método Bicicleta', async ({ page }) => {
+    const deliverPage = new DeliverPage(page)
+    const utilidades = new Utilidades(page)
 
-    await test.step('selecionar o método de entrega', async () => {
-      await page.locator('li > span').first().click()
-      await page.locator('li[class="selected"]').isVisible()
-    })
+    const fakerUsuario = await utilidades.gerarDadosDoUsuario()
+    const fakerEndereco = await utilidades.gerarDadosEndereco()
 
-    await test.step('upload da CNH', async () => {
-      await expect(page.locator('.dropzone > p')).toContainText('Foto da sua CNH')
-      await page.locator('input[type="file"]').setInputFiles('utilidades/imagem/cnh.jpg')
-      await page.locator('div[class="dropzone"] img').isVisible()
+    await deliverPage.preencherDadosDoUsuario({
+      nomeCompleto: fakerUsuario.nomeCompleto,
+      cpf: fakerUsuario.cpf,
+      email: fakerUsuario.email,
+      telefone: fakerUsuario.telefone
     })
+    await deliverPage.preencherDadosDoEndereco({
+      cep: fakerEndereco.cep,
+      numero: fakerEndereco.numero,
+      complemento: fakerEndereco.complemento
+    })
+    await deliverPage.selecionarMetodoEntrega('Bicicleta')
+    await deliverPage.efetuarUploadCnh('utilidades/imagem/cnh.jpg')
+    await deliverPage.efetuarCadastro()
+  })
 
-    await test.step('efetuar cadastro', async () => {
-      await page.locator('.button-success').click()
-      await expect(page.locator('#swal2-title')).toContainText('Aí Sim...')
-      await page.locator('button[class="swal2-confirm swal2-styled"]').click()
-      await expect(page.locator('main > h1')).toContainText('Seja um parceiro entregador pela Buger Eats')
+  test('Deve cadastrar entregador para o método Van/Carro', async ({ page }) => {
+    const deliverPage = new DeliverPage(page)
+    const utilidades = new Utilidades(page)
+
+    const fakerUsuario = await utilidades.gerarDadosDoUsuario()
+    const fakerEndereco = await utilidades.gerarDadosEndereco()
+
+    await deliverPage.preencherDadosDoUsuario({
+      nomeCompleto: fakerUsuario.nomeCompleto,
+      cpf: fakerUsuario.cpf,
+      email: fakerUsuario.email,
+      telefone: fakerUsuario.telefone
     })
+    await deliverPage.preencherDadosDoEndereco({
+      cep: fakerEndereco.cep,
+      numero: fakerEndereco.numero,
+      complemento: fakerEndereco.complemento
+    })
+    await deliverPage.selecionarMetodoEntrega('Van/Carro')
+    await deliverPage.efetuarUploadCnh('utilidades/imagem/cnh.jpg')
+    await deliverPage.efetuarCadastro()
   })
 })
---
+++ utilidades/utilidades.js
@@ -0,0 +1,42 @@
+const faker = require('faker-br')
+
+exports.Utilidades = class Utilidades {
+
+    /**
+     * @param {import('@playwright/test').Page} page
+     */
+    constructor(page) {
+        this.page = page
+    }
+
+    async gerarDadosDoUsuario() {
+        const cpf = faker.br.cpf()
+        const nomeCompleto = `${faker.name.firstName()} ${faker.name.lastName()}`
+        const email = faker.internet.email()
+        // Telefone está sendo gerado dessa forma para poder mostrar array no projeto com alguns DDD
+        const dddsValidos = ['11', '21', '31', '41', '48', '61']
+        const dddEstado = faker.random.arrayElement(dddsValidos)
+        const numeroTelefone = faker.random.number({ min: 10000000, max: 99999999 })
+        const telefone = `(${dddEstado})9${numeroTelefone}`
+        return {
+            cpf,
+            nomeCompleto,
+            email,
+            telefone
+        }
+    }
+
+    async gerarDadosEndereco() {
+        const cep = faker.address.zipCodeValidByState()
+        //A função match() retorna um array contendo todas as correspondências encontradas
+        // e o método join('') combina todos os dígitos encontrados em uma única string, 
+        // eliminando quaisquer espaços ou outros caracteres não numéricos.
+        const numero = faker.address.streetAddress().match(/\d+/g).join('')
+        const complemento = `Apto: ${faker.random.number({ min: 1, max: 300 })} Bloco: ${faker.random.number({ min: 1, max: 2 })}`
+        return {
+            cep,
+            numero,
+            complemento
+        }
+    }
+}
